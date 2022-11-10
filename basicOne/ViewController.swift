94f151110459aea3e5439196a19c5dd04f981e08
Akhil Surendran <akhilsurendranvr@gmail.com>
2022-11-10 13:19:41 +0530

    Initial Commit

---
+++ basicOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicOne
+//
+//  Created by ddukk15 on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
